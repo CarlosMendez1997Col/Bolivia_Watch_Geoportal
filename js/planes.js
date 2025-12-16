b26ccaad9f5e63e1f8d3f1a975feb1144a6d3262
Carlos Mendez <carlos.mendez@unesp.br>
2025-12-16 10:14:30 -0500

    Create planes.js

---
+++ js/planes.js
@@ -0,0 +1,10 @@
+/*=====================================================================================================================================================*/
+/*=====                                                        Bolivia Watch Geoportal                                                            =====*/
+/*=====                                         Stockholm Environment Institute - Latin America (SEI-LA)                                          =====*/
+/*=====                                              Copyright 2025, Stockholm Environment Institute                                              =====*/
+/*=====                                            https://github.com/sei-latam/Bolivia_Watch_Geoportal                                           =====*/
+/*=====                                            https://sei-latam.github.io/Bolivia_Watch_Geoportal/                                           =====*/
+/*=====                                                                Planes.js                                                                  =====*/
+/*=====                                          Geoportal Developed by Carlos Mendez - Research Associate I                                      =====*/
+/*=====                                                              Apache-2.0 license                                                           =====*/
+/*=====================================================================================================================================================*/
